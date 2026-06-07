c7d5098905a7b13f36d67011e0c4c333926a46a3
Enzo <sc.enzobusiness02@gmail.com>
2026-06-07 16:08:17 +0200

    Ejercicio 4 completado

--- Ejercicios básicos y avanzados/ejercicio-03.js
+++ Ejercicios básicos y avanzados/ejercicio-03.js
@@ -1,23 +1,23 @@
-console.log(10 * 5); /* 3.1 */
-console.log(10 / 2); /* 3.2 */
-console.log(15 % 9); /* 3.3 */
+console.log(10 * 5); // 3.1
+console.log(10 / 2); // 3.2
+console.log(15 % 9); // 3.3
 
 let p = 10;
 let j = 5;
 
-let o = p; /* la variable o guarda el valor de p -> 10 */
-o += j; /* sumamos el valor de j -> 5 */
+let o = p; // la variable o guarda el valor de p -> 10
+o += j; // sumamos el valor de j -> 5
 
-console.log(o); /* 3.4 */
+console.log(o); // 3.4
 
 
 let c = 10; 
 let m = 5;
 
-let i = m; /* la variable i guarda el valor de m -> 5 */
-i *= c; /* multiplicamos el valor de c -> 10 */
+let i = m; // la variable i guarda el valor de m -> 5
+i *= c; // multiplicamos el valor de c -> 10
 
-console.log(i); /* 3.5 */
+console.log(i); // 3.5
 
 
 
