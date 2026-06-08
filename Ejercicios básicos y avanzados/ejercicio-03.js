888c27e29e606b294f8d2639838b1490aada6d78
Enzo <sc.enzobusiness02@gmail.com>
2026-06-08 11:14:53 +0200

    Ejercicio 3 completado

--- Ejercicios básicos y avanzados/ejercicio-03.js
+++ Ejercicios básicos y avanzados/ejercicio-03.js
@@ -1,6 +1,14 @@
+// En este ejercicio podéis mostrar directamente los resultados por consola o almacenarlos en variables (esto lo podéis hacer en todos los demás, como os resulte más claro).
+
+// 3.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+// 3.2 Divide 10 por 2 y muestra el resultado en un console.
+// 3.3 Muestra mediante un console el resto de dividir 15 por 9.
+
 console.log(10 * 5); // 3.1
 console.log(10 / 2); // 3.2
 console.log(15 % 9); // 3.3
+
+// 3.4 Usa el correcto operador de asignación que resultará en o = 15, teniendo dos variables p = 10 y j = 5.
 
 let p = 10;
 let j = 5;
@@ -10,6 +18,7 @@ o += j; // sumamos el valor de j -> 5
 
 console.log(o); // 3.4
 
+// 3.5 Usa el correcto operador de asignación que resultará en i = 50, teniendo dos variables c = 10 y m = 5.
 
 let c = 10; 
 let m = 5;
