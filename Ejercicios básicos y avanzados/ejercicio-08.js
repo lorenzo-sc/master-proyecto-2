d0a06c83dc1a3bc366af871ce58404a3730a4e01
Enzo <sc.enzobusiness02@gmail.com>
2026-06-08 10:05:29 +0200

    Ejercicio 8 completado con la respuesta ceñida al enunciado y mi interpretación

--- Ejercicios básicos y avanzados/ejercicio-08.js
+++ Ejercicios básicos y avanzados/ejercicio-08.js
@@ -12,15 +12,40 @@ const avengers = [
     "Captain M.",
 ];
 
-function findLongestWord(stringList){
+// Aquí la respuesta que se ciñe al enunciado
+
+function findLongestWord(stringList) {
+    let longestWord = "";
+    for (let word of stringList) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    }
+    return longestWord;
+};
+
+console.log('La palabra que supuestamente es la más larga (ciñiendonos al enunciado) es -> "' + findLongestWord(avengers) + '"');
+
+// El enunciado no lo pide, pero entiendo que al hablar de "la palabra más larga" lo justo es comparar solo los caracteres de cada palabra, así que descarto espacios y puntos antes de medir su longitud.
+
+// Entonces para mi esta sería la resolución correcta:
+
+function findRealLongestWord(stringList){ //la voy a llamar findRealLongestWord
     let LongestWord = ""; // Declaramos la variable LongestWord
 
     for (let Word of stringList){ //declaramos la variable Word y la asociamos en la función for
-    
-    if ( Word.length > LongestWord.length){ // si el tamaño de Word es mayor que el de LongestWord
+
+    let CleanWord = Word.replaceAll(" ","").replaceAll(".",""); //reemplazamos los valores de Word por su versión sin espacios y sin puntos (CleanWord)
+    let CleanLongest = LongestWord.replaceAll(" ","").replaceAll(".",""); //reemplazamos los valores de LongestWord por su versión sin espacios y sin puntos (CleanLongest)
+
+
+    if ( CleanWord.length > CleanLongest.length){ // si el tamaño de Word (sin espacios ni puntos) es mayor que el de LongestWord (sin espacios ni puntos)
         LongestWord = Word}   // La palabra (Word) será la más larga (LongestWord)
     }
     return (LongestWord); //devolvemos la palabra más larga
 };
 
-console.log(findLongestWord(avengers)); // llamamos a la función para que se ejecute en la consola respecto a la constante "avengers" que se definió al inicio
+
+console.log('La palabra realmente más larga es -> "' + findRealLongestWord(avengers) + '"'); // llamamos a la función para que se ejecute en la consola respecto a la constante "avengers" que se definió al inicio
+
+
