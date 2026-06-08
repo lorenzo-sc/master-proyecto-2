d3bcb6b8cb3e8780390063ca6fe77dbf9d3a70ab
Enzo <sc.enzobusiness02@gmail.com>
2026-06-08 11:01:19 +0200

    Ejercicio 5 completado

--- Ejercicios básicos y avanzados/ejercicio-05.js
+++ Ejercicios básicos y avanzados/ejercicio-05.js
@@ -36,3 +36,4 @@ if (number3 * 5 == number1 && number1 * 2 == number2) {
 if (number2 / 2 == number1 || number1 / 5 == number3) {
     console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
 }
+
