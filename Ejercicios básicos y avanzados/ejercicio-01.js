7f32a875991cc59e8c9c1d716de7550c49ab15ad
Enzo <sc.enzobusiness02@gmail.com>
2026-06-08 11:07:50 +0200

    Ejercicio 1 completado

--- Ejercicios básicos y avanzados/ejercicio-01.js
+++ Ejercicios básicos y avanzados/ejercicio-01.js
@@ -1,9 +1,22 @@
+// 1. Define tu superhéroe favorito:
+
+// Crea una variable llamada miSuperheroeFavorito y asigna el valor "Hulk".
+
 const miSuperheroeFavorito = "Hulk";
 console.log(miSuperheroeFavorito);
+
+// 2. Define un número:
+
+// Crea una variable llamada numeroFavorito y asigna el valor 50.
 
 let numeroFavorito
 numeroFavorito = 50;
 console.log(numeroFavorito);
+
+// 3. Define las dimensiones:
+
+// Crea una variable llamada altura y asigna el valor 5.
+// Crea una variable llamada longitud y asigna el valor 10.
 
 let altura
 altura = 5;
@@ -13,6 +26,15 @@ let longitud
 longitud = 10;
 console.log(longitud);
 
+// 4. Calcula la suma:
+// Crea una variable llamada suma y asigna el valor de altura + longitud.
+
 let suma
 suma = altura + longitud;
 console.log(suma);
+
+// 5. Imprime los resultados:
+// Muestra todas las variables por consola.
+
+console.log(miSuperheroeFavorito + " " + numeroFavorito + " " + altura + " " + longitud + " " + suma);
+
