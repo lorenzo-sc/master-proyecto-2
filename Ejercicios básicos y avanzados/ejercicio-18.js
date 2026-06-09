d95e8a950e2b7f43e5208e375a6302a76e70a438
Enzo <sc.enzobusiness02@gmail.com>
2026-06-09 11:25:29 +0200

    Ejercicio 18 completado

---
+++ Ejercicios básicos y avanzados/ejercicio-18.js
@@ -0,0 +1,23 @@
+// Usa un bucle for para recorrer todos los destinos del array y elimina los elementos que tengan el id 11 y el id 40.
+
+// Imprime en un console log el array resultante.
+
+// Puedes usar este array:
+
+const placesToTravel = [
+  { id: 5, name: "Japan" },
+  { id: 11, name: "Venecia" },
+  { id: 23, name: "Murcia" },
+  { id: 40, name: "Santander" },
+  { id: 44, name: "Filipinas" },
+  { id: 59, name: "Madagascar" },
+];
+
+let SitiosOptimos = [];
+
+for (let valor of placesToTravel){
+    if ((valor.id != 11) && (valor.id != 40))
+        {SitiosOptimos.push(valor)} // mete "valor" dentro de SitiosOptimos
+};
+
+console.log(SitiosOptimos)
