9a065d9f5d73bdff6909c224634828c8cc47beb8
Enzo <sc.enzobusiness02@gmail.com>
2026-06-13 09:56:04 +0200

    Ejercicio 24 completado

--- Ejercicios básicos y avanzados/ejercicio-24.js
+++ Ejercicios básicos y avanzados/ejercicio-24.js
@@ -3,15 +3,15 @@
 // Imprime este nuevo array por consola.
 
 const characters = [
-  { name: 'Luke Skywalker', species: 'Human' },
-  { name: 'Darth Vader', species: 'Human' },
-  { name: 'Chewbacca', species: 'Wookiee' },
-  { name: 'Leia Organa', species: 'Human' },
-  { name: 'R2-D2', species: 'Droid' },
-  { name: 'C-3PO', species: 'Droid' },
-  { name: 'Obi-Wan Kenobi', species: 'Human' },
-  { name: 'Yoda', species: 'Unknown' },
-  { name: 'Han Solo', species: 'Human' }
+    { name: 'Luke Skywalker', species: 'Human' },
+    { name: 'Darth Vader', species: 'Human' },
+    { name: 'Chewbacca', species: 'Wookiee' },
+    { name: 'Leia Organa', species: 'Human' },
+    { name: 'R2-D2', species: 'Droid' },
+    { name: 'C-3PO', species: 'Droid' },
+    { name: 'Obi-Wan Kenobi', species: 'Human' },
+    { name: 'Yoda', species: 'Unknown' },
+    { name: 'Han Solo', species: 'Human' }
 ];
 const humanCharacters = [];
 
