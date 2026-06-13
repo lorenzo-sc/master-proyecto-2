bc348e298ae8153f553a3939c5640f362f3fc9a6
Enzo <sc.enzobusiness02@gmail.com>
2026-06-13 09:55:18 +0200

    Ejercicio 25 completado

---
+++ Ejercicios básicos y avanzados/ejercicio-25.js
@@ -0,0 +1,44 @@
+// Dada una lista de películas, cuenta cuántas son de antes del año 2000 y cuántas son posteriores, utilizando un bucle.
+
+// Imprime ambos conteos por consola.
+
+const movies = [
+    { title: 'The Matrix', releaseYear: 1999 },
+    { title: 'Star Wars: Episode IV – A New Hope', releaseYear: 1977 },
+    { title: 'Inception', releaseYear: 2010 },
+    { title: 'Jurassic Park', releaseYear: 1993 },
+    { title: 'The Shawshank Redemption', releaseYear: 1994 },
+    { title: 'Pulp Fiction', releaseYear: 1994 },
+    { title: 'Avatar', releaseYear: 2009 },
+    { title: 'The Dark Knight', releaseYear: 2008 },
+    { title: 'Fight Club', releaseYear: 1999 },
+    { title: 'Forrest Gump', releaseYear: 1994 }
+];
+
+let contador_antiguas = 0;
+let contador_modernas = 0;
+
+const Peliculas_Antiguas = [];
+const Peliculas_Modernas = [];
+
+for (Peli of movies){
+    if (Peli.releaseYear < 2000){
+        Peliculas_Antiguas.push(Peli.title + " -> " + Peli.releaseYear)
+        contador_antiguas++
+    }
+    else{
+        Peliculas_Modernas.push(Peli.title + " -> " + Peli.releaseYear)
+        contador_modernas++}
+};
+
+console.log(" ");
+
+console.log("Hay un total de "+ contador_antiguas + " películas anteriores al previas al 2000: ");
+console.log(Peliculas_Antiguas);
+
+console.log(" ");
+
+console.log("Hay un total de "+ contador_modernas + " películas posteriores al 2000: ");
+console.log(Peliculas_Modernas);
+
+console.log(" ");
